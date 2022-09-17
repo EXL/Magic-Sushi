98f6002eddeb8eb3683d1cf416a6193c78ccef63
EXL <exlmotodev@gmail.com>
2022-09-17 17:22:34 +0700

    Update creating MPKG-package script

--- Ezx/Create-MPKG-Package.sh
+++ Ezx/Create-MPKG-Package.sh
@@ -2,18 +2,21 @@
 
 cd "${0%/*}"
 
-rm -Rf magic-sushi
-mkdir magic-sushi
+rm -Rf Magic-Sushi
+rm -Rf *.pkg
+rm -Rf *.mpkg
 
-cp -R ../Assets magic-sushi
-cp ../Magic-Sushi-EZX magic-sushi
+mkdir Magic-Sushi
 
-cp Magic-Sushi.desktop magic-sushi
-cp ../Images/Icons/Magic-Sushi-EZX-IconBig.png magic-sushi
-cp ../Images/Icons/Magic-Sushi-EZX-IconSmall.png magic-sushi
+cp -R ../Assets Magic-Sushi
+cp ../Magic-Sushi-EZX Magic-Sushi
 
-tar --numeric-owner --owner=2000 --group=233 -cvf magic-sushi.uncompressed magic-sushi
-gzip -9f magic-sushi.uncompressed
+cp Magic-Sushi.desktop Magic-Sushi
+cp ../Images/Icons/Magic-Sushi-EZX-IconBig.png Magic-Sushi
+cp ../Images/Icons/Magic-Sushi-EZX-IconSmall.png Magic-Sushi
 
-rm -Rf magic-sushi
-mv magic-sushi.uncompressed.gz Magic-Sushi-EZX_v1.0.0_17-Sep-2022.mpkg
+tar --numeric-owner --owner=2000 --group=233 -cvf Magic-Sushi.uncompressed Magic-Sushi
+gzip -9f Magic-Sushi.uncompressed
+mv Magic-Sushi.uncompressed.gz Magic-Sushi-EZX_v1.0.0_17-Sep-2022.mpkg
+
+rm -Rf Magic-Sushi
